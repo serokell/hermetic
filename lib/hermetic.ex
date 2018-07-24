7ae8fac92fe2c463afb3f3a5fde62bb73ecc4c19
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-07-24 05:08:57 +0300

    Merge Hermetic and Hermetic.Supervisor

--- lib/hermetic.ex
+++ lib/hermetic.ex
@@ -1,27 +1,18 @@
 defmodule Hermetic do
   use Application
 
-  def start(_type, _args) do
-    Hermetic.Supervisor.start_link(name: Hermetic.Supervisor)
-  end
-end
-
-defmodule Hermetic.Supervisor do
-  use Supervisor
-
-  def start_link(opts) do
-    Supervisor.start_link(__MODULE__, :ok, opts)
+  def slack_token do
+    Application.get_env(:hermetic, :slack_token)
   end
 
-  def init(:ok) do
+  def start(_, _) do
     children = [
       {YTCache, name: YTCache},
       %{
         id: Slack.Bot,
-        start: {Slack.Bot, :start_link, [SlackRtm, [], Application.get_env(:hermetic, :slack_token)]}
+        start: {Slack.Bot, :start_link, [SlackRtm, [], slack_token()]}
       }
     ]
-
-    Supervisor.init(children, strategy: :one_for_one)
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
