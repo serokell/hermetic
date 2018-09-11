ca9dc3c028f74a44277fe1e640859e1d7f39dd6e
Lars Jellema <lars.jellema@gmail.com>
2018-09-11 12:29:11 +0200

    Move port to config.exs

--- lib/hermetic.ex
+++ lib/hermetic.ex
@@ -3,11 +3,14 @@ defmodule Hermetic do
   Slack bot that links to YouTrack issues.
   """
 
+  import ConfigMacro
+  config :hermetic, [:cowboy_options]
+
   use Application
 
   def start(_, _) do
     children = [
-      {Plug.Adapters.Cowboy2, plug: Hermetic.Router, scheme: :http, options: [port: 8080]},
+      {Plug.Adapters.Cowboy2, plug: Hermetic.Router, scheme: :http, options: cowboy_options()},
       Hermetic.YouTrack.ProjectID,
       Hermetic.YouTrack.EmailsToLogins,
     ]
