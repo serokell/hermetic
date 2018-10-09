437edf144222cda6d2fe2778f1c22d90568a35e0
Lars Jellema <lars.jellema@gmail.com>
2018-10-09 20:27:42 +0200

    Move split/1 to Hermetic.Slash

--- lib/hermetic/slash.ex
+++ lib/hermetic/slash.ex
@@ -6,6 +6,39 @@ defmodule Hermetic.Slash do
   """
 
   @doc ~S"""
+  Split a string into whitespace delimited or quoted parts.
+
+      iex> split("Foo foon't \"bar bazn't\"")
+      ["Foo", "foon't", "bar bazn't"]
+  """
+  @spec split(String.t()) :: [String.t()]
+  def split(string) do
+    do_split(String.trim_leading(string, " "), "", [], false)
+  end
+
+  # Any character after a backslash is taken literally
+  @spec do_split(String.t(), String.t(), [String.t()], boolean) :: [String.t()]
+  defp do_split(<<?\\, c, rest::binary>>, buf, acc, quoting),
+    do: do_split(rest, <<buf::binary, c>>, acc, quoting)
+
+  # Toggle quoting on double quote
+  defp do_split(<<?", rest::binary>>, buf, acc, quoting),
+    do: do_split(rest, buf, acc, !quoting)
+
+  # Start a new segment after an unquoted space
+  defp do_split(<<?\s, rest::binary>>, buf, acc, false),
+    do: do_split(String.trim_leading(rest, " "), "", [buf | acc], false)
+
+  # Any other character is simply copied
+  defp do_split(<<c, rest::binary>>, buf, acc, quoting),
+    do: do_split(rest, <<buf::binary, c>>, acc, quoting)
+
+  # Finish string or raise if unclosed
+  defp do_split(<<>>, "", acc, false), do: Enum.reverse(acc)
+  defp do_split(<<>>, buf, acc, false), do: Enum.reverse([buf | acc])
+  defp do_split(<<>>, _, _, _), do: raise("quoted string not closed")
+
+  @doc ~S"""
   Translate a Slack user id to a YouTrack login name.
   """
   @spec translate_user_id(String.t()) :: String.t()
